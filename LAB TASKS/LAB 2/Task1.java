77f72c85c36495973f885013ce1a37f194785763
Muhammad Daniyal <102672505+denix-developer@users.noreply.github.com>
2022-10-11 06:51:40 -0700

    Add files via upload

---
+++ LAB TASKS/LAB 2/Task1.java
@@ -0,0 +1,13 @@
+public class Task1{
+    public static void main(String[] args) {
+        float a,b,c,d,e,f;
+        a = 5.1f;
+        b = 4.1f;
+        c = 3.1f;
+        d = 2.1f;
+        e = 1.1f;
+        f= a+b+c+d+e;
+        System.out.println("Sum of all floating numbers is " + f);
+        f = f / 5f;
+        System.out.println("Average of all floating numbers is " + f);
+    }}
