e5b7aa834b2c8400d106021cff856e9df5c561a7
Muhammad Daniyal <102672505+denix-developer@users.noreply.github.com>
2022-10-11 07:33:59 -0700

    Add files via upload

---
+++ LAB TASKS/LAB 6/Task1.java
@@ -0,0 +1,22 @@
+public class Task1{
+    public static void main(String[] args) {
+        int array[]= {1,2,4,5,7,8,10,22,11,15};
+        System.out.println("Please enter a number");
+        int size=array.length+1;
+        int input=Integer.parseInt(args[0]);
+        for(int i=0;i<size;i++) {
+
+            if (i==size-1){
+                System.out.println("This number is not an element of the array");
+                break;}
+
+
+            else if(input==array[i]) {
+                System.out.println("The index of this element "+array[i]+" in the array is "+i);
+                break;
+            }
+
+
+        }
+    }
+}
---
+++ LAB TASKS/LAB 6/Task2.java
@@ -0,0 +1,13 @@
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        String Rollno,Name;
+        Name=args[0];
+        Rollno=args[1];
+        System.out.println("Your Name is "+Name);
+        System.out.println("Your Rollno is "+Rollno);
+
+    }
+
+}
