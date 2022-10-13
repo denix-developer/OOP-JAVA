6b14ebb110bbd8dc06688e5fe1dd114225f38746
Muhammad Daniyal <102672505+denix-developer@users.noreply.github.com>
2022-10-13 04:49:36 -0700

    Add files via upload

---
+++ LAB TASKS/LAB 7/Task1.java
@@ -0,0 +1,13 @@
+public class Task1{
+
+    public static void main(String[] args) {
+        int b=0;
+        int a=5;
+        try {
+            System.out.println(a / b); //
+        }
+        catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ LAB TASKS/LAB 7/Task3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class MyException extends Exception
+{
+    public MyException(String str)
+    {
+        System.out.println(str);
+    }
+}
+
+public class Task3{
+    public static void main(String[] args)throws MyException{
+        Scanner sc = new Scanner(System.in);
+        int i;
+        System.out.println("Input a number");
+        i=sc.nextInt();
+
+        try{
+            if(i<0) {
+            }
+            throw new MyException("The number is negative");
+        }
+        catch(MyException e) {
+            System.out.println(e);
+        }
+    }
+}
